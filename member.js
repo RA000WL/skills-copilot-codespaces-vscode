c5adcc362cac39b1177cf3a08e320d0bf25bc4b4
Rahul Magar <rahulbicte79@jantacampusith.edu.np>
2024-10-18 07:28:12 +0000

    second copilot commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMembers() {
+    return [
+        {name: 'John', skills: ['JavaScript', 'React', 'Node']},
+        {name: 'Jane', skills: ['JavaScript', 'Angular', 'Node']},
+        {name: 'Jack', skills: ['HTML', 'CSS', 'JavaScript']}
+    ];
+}
